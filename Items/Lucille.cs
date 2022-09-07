1cde7f1227a27e08b66d765034a9a20cfdae0558
Liam Hodgson <limeman42@gmail.com>
2022-09-07 10:24:38 -0700

    Changed icon, added items & projectiles folders, added Lucille item and sprite, added Lucille item in game (currently has no properties).

---
+++ Items/Lucille.cs
@@ -0,0 +1,29 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace mushroommod.Items
+{
+    public class Lucille : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Jeffery, break out Lucille");
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.maxStack = 1;
+            Item.value = 100;
+            Item.rare = ItemRarityID.Pink;
+            // Set other Item.X values here
+        }
+
+        public override void AddRecipes()
+        {
+            // Recipes here. See Basic Recipe Guide
+        }
+    }
+}
