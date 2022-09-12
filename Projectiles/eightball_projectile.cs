cb78f716e94055e9f61079d1ccb1181a6a747817
Liam Hodgson <limeman42@gmail.com>
2022-09-12 12:11:35 -0700

    see changelog, 9/12/22

---
+++ Projectiles/eightball_projectile.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace mushroommod.Projectiles
+{
+    public class eightball_projectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Eight Ball");
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.arrow = true;
+            Projectile.width = 10;
+            Projectile.height = 10;
+            Projectile.aiStyle = ProjAIStyleID.Arrow; // or 1
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Melee;
+            AIType = ProjectileID.WoodenArrowFriendly;
+        }
+
+        // Additional hooks/methods here.
+    }
+}
