6d59e628f6b6cde7995456ec355b46ca54ff0bc7
Liam Hodgson <limeman42@gmail.com>
2022-10-12 09:58:09 -0700

    see changelog 10/12/22

---
+++ NPCs/Lizard_Wizard.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace mushroommod.NPCs
+{
+    public class Lizard_Wizard : ModNPC
+    {
+
+        public override void SetStaticDefaults()
+        {
+
+        }
+
+        public override void SetDefaults()
+        {
+            // general
+            NPC.height = 40;
+            NPC.width = 18;
+            NPC.aiStyle = 7;
+            NPC.friendly = true;
+            NPC.townNPC = true;
+
+            // combat
+            NPC.lifeMax = 250;
+            NPC.damage = 10;
+            NPC.defense = 15;
+            NPC.knockBackResist = 0.5f;
+
+        }
+    
+    }
+}
